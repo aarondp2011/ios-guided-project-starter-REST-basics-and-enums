b54781619af28740ff7f83fea647a68ed8a8ad54
Aaron Peterson <aarondp2011@gmail.com>
2020-05-05 22:26:55 -0500

    Final commit

--- FindACrew/Model Controllers/PersonController.swift
+++ FindACrew/Model Controllers/PersonController.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 class PersonController {
+    
+    var crew: [Person] = []
+    
     enum HTTPMethod: String {
         case get = "GET"
         case put = "PUT"
@@ -18,4 +21,58 @@ class PersonController {
     
     private let baseURL = URL(string: "https://lambdaswapi.herokuapp.com")!
     private lazy var peopleURL = URL(string: "/api/people", relativeTo: baseURL)!
+    
+    func searchFoePeopleWith(searchTerm: String, completion: @escaping () -> Void) {
+        
+        // Step 1: Build Endpoint URL with query items
+        var urlComponents = URLComponents(url: peopleURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            print ("request URL is nil")
+            completion()
+            return
+        }
+        
+        // Step 2: Create URL Request
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        // Step 3: Create URL Task
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            // Handle Error First
+            if let error = error {
+                print("Error fetching data: \(error)")
+                completion()
+                return
+            }
+            
+            guard let self = self else { completion(); return }
+            
+            // Handle Data Optionality
+            guard let data = data else {
+                print("Ni data returned from data task.")
+                completion()
+                return
+            }
+            
+            // Create Decoder
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            // Decode and adding object to array
+            do {
+                let personSearch = try jsonDecoder.decode(PersonSearch.self, from: data)
+                self.crew.append(contentsOf: personSearch.results)
+            } catch {
+                print("Unable to decode data into object of typr PersonSearch: \(error)")
+            }
+            
+            completion()
+        }
+        
+        // Step 4: Run URL Task
+        task.resume()
+    }
 }
